dfb891504a3e39598c32711c593394765b71e1a8
Sierra MacDonald <sierrainertia@gmail.com>
2020-09-11 22:25:58 -0400

    refacture code

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
+//store select with id pokemonType
 let pokemonTypeSelect = document.getElementById("pokemonType");
+// set length to 0
 pokemonTypeSelect.length = 0;
+// Create element option for initial text
 let pokemonTypeOption = document.createElement("option");
 pokemonTypeOption.text = "Pick a type";
 
-const url = "https://pokeapi.co/api/v2/type/";
+// API Type URL
+const typeUrl = "https://pokeapi.co/api/v2/type/";
 
+// add pokemonTypeOption to pokemonTypeSelect
 pokemonTypeSelect.add(pokemonTypeOption);
 pokemonTypeSelect.selectedIndex = 0;
+
 // Make Call to API
-fetch(url)
-  .then(function (response) {
-    if (response.status !== 200) {
-      console.warn("looks like there was a problem");
-      return;
+fetch(typeUrl).then(function (response) {
+  // if the API returns a non successful status error
+  if (response.status !== 200) {
+    console.warn("looks like there was a problem");
+    return;
+  }
+  response.json().then(function (data) {
+    let option;
+    // loop through the results length and each time create an option with the value from that data
+    for (let i = 0; i < data.results.length; i++) {
+      option = document.createElement("option");
+      option.text = data.results[i].name;
+      option.value = data.results[i].name;
+      // add the option to the list
+      pokemonTypeSelect.add(option);
     }
-    response.json().then(function (data) {
-      //   console.log(data.results);
-      let option;
-      for (let i = 0; i < data.results.length; i++) {
-        option = document.createElement("option");
-        option.text = data.results[i].name;
-        option.value = data.results[i].name;
-        pokemonTypeSelect.add(option);
-      }
-    });
-  })
-  .catch(function (error) {
-    // if theres an error getting api
-    console.warn("something went wrong", error);
   });
+});
